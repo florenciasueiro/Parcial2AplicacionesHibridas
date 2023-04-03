549b5008e682fc78ac8788fb5b8882302e3189da
franGhisoni <ghisonifran@gmail.com>
2023-04-03 11:55:28 -0300

    css card linked

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -4,13 +4,14 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
-
+import CardCSS from '../css/Card.module.css';
 
 export default function ActionAreaCard() {
   return (
-    <Card sx={{ maxWidth: 345 }}>
+    <Card sx={{ maxWidth: 345 }} className={CardCSS.cards}>
       <CardActionArea>
-        <CardMedia
+        <CardMedia 
+          className={CardCSS.image}
           component="img"
           height="140"
           image="/img/mano-que-sostiene-cubo-madera-destino.jpg"
@@ -18,8 +19,10 @@ export default function ActionAreaCard() {
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
+            
             Mision y vision
           </Typography>
+          
           <Typography variant="body2" color="text.secondary">
             Lizards are a widespread group of squamate reptiles, with over 6,000
             species, ranging across all continents except Antarctica
