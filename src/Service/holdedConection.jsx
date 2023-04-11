6375d6248c2d6495681347bff53e1caadd017c81
franGhisoni <ghisonifran@gmail.com>
2023-04-11 15:08:07 -0300

    comentarios y fix navbar

--- src/Service/holdedConection.jsx
+++ src/Service/holdedConection.jsx
@@ -1,97 +1,24 @@
 import React, { useState, useEffect, useCallback } from 'react';
-import CtCSS from '../css/Contacto.module.css';
 
-//const
-// const options = {
-//   method: 'GET',
-//   headers: {accept: 'application/json', key: '343654e3d1014f792344a19ee8f40503'}
-// };
-// https://api.holded.com/api/invoicing/v1/contacts
-// fetch('https://api.holded.com/api/invoicing/v1/contacts?customId=testmail%40gmail.com%3Bpassword', options)
-//   .then(response => response.json())
-//   .then(response => console.log(response))
-//   .catch(err => console.error(err));
+//esta funcion es la que llama el login form para enviar los datos de email y password al api
+// de que trae la lista de contactos pero usamos el filtro de customid para que solo traiga el
+// usuario que tenga ese mismo customid.
 
 
-//   function LoginForm() {
-//     const [username, setUsername] = useState('');
-//     const [password, setPassword] = useState('');
-  
-//     const handleSubmit = async (event) => {
-//       event.preventDefault();
-  
-//       const response = await fetch('https://api.holded.com/api/invoicing/v1/contacts'+username+password, {
-//         method: 'POST',
-//         headers: { 'Content-Type': 'application/json' },
-//         body: JSON.stringify({ username, password }),
-//       });
-  
-//       if (response.ok) {
-//         // El usuario ha sido autenticado correctamente, redirige a la página correspondiente
-//         window.location.href = '/dashboard';
-//       } else {
-//         // Mostrar un mensaje de error en la pantalla
-//         const error = await response.json();
-//         alert(error.message);
-//       }
-//     };
-  
-//     return (
-//       <form onSubmit={handleSubmit}>
-//         <label>
-//           Nombre de usuario:
-//           <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-//         </label>
-//         <label>
-//           Contraseña:
-//           <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-//         </label>
-//         <button type="submit">Iniciar sesión</button>
-//       </form>
-//     );
-//   }
-  
-//   export function LoginForm();
+// ya que en holded no hay un campo de contraseña decidi usar el custom id como la suma de email y password
 
-// export default function login(email, password) {
-//   const url = "https://api.holded.com/api/invoicing/v1/contacts";
-//   const customid = `${email.replace(/@/g, "%40")}%3B${password}`;
-//   const options = {
-//     method: "GET",
-//     body: JSON.stringify({customid:customid}),
-//  headers: {
-//             'customid':customid,
-//             'key': '343654e3d1014f792344a19ee8f40503',
-//             'accept': 'application/json',
-//             'Acces-Control-Allow-Origin': '*'
-//           }
-//   };
 
-//   fetch(url, options)
-//     .then((response) => {
-//       if (!response.ok) {
-//         throw new Error("Network response was not ok");
-//       }
-//       return response.json();
-//     })
-//     .then((data) => {
-//         console.log(data);
-//       // hacer algo con la respuesta
-//     })
-//     .catch((error) => {
-//       console.error("There was a problem with the fetch operation:", error);
-//     });
-// }
-
-// export default login;
 export default function useContactos(){
 const contactos= useCallback(async (email, password) => {
-  
+  //   aca lo que hago es remplazar los caracteres que por lo visto no se peuden mandar en un http request
   const customid = `${email.replace(/@/g, "%40")}%3B${password}`;
+  //borrar
   console.log(customid);
+  //   todo en un bloque try para robustez
     try {
         const response = await  fetch(`https://api.holded.com/api/invoicing/v1/contacts?customId=${customid}`, {
           headers: {
+            // todavia no esta resuelto lo del cors asi que el acces control no esta haciendo nada
             'customid':customid,
             'key': '343654e3d1014f792344a19ee8f40503',
             'accept': 'application/json',
@@ -104,17 +31,22 @@ const contactos= useCallback(async (email, password) => {
             }
 
         const data = await response.json();
+        // borrar todos los console log
             console.log(data);
             console.log("data[0]");
             console.log(data[0]);
             console.log("JSON.stringify(data)");
             console.log(JSON.stringify(data));
+            // guardo el user en un sessionStorgae
             
             sessionStorage.setItem("user", JSON.stringify(data[0]));
+            // no elimine este por que fue el primer intentno y no estoy seguro si lo sigo usando en algun otro lugar
             sessionStorage.setItem("userName", JSON.stringify(data[0].name));
             console.log(data[0].name);
             console.log("sessionStorage.getItem('user')");
             console.log(sessionStorage.getItem("user"));
+            // una vez se ejecuta esto refresco la pagina para que se actualice la palabra login pues no se como hacerlo de otra manera
+
             window.location.reload();
         }catch(error){
             console.error("There was a problem with the fetch operation:", error);
@@ -122,47 +54,5 @@ const contactos= useCallback(async (email, password) => {
         }, []);
         
         return contactos;
-    //     }).then((resp)=> {
-    //         console.log(resp.json());
-    //     })
-    //     .catch((error)=> {
-    //         console.log("There was a problem with the fetch operation: ", error)
-    //     });
-    // },[]);
-    // return contactos;
+
 }
-// import React, { useState } from "react";
-
-// function LoginForm({ login }) {
-//   const [email, setEmail] = useState("");
-//   const [password, setPassword] = useState("");
-
-//   const handleSubmit = (event) => {
-//     event.preventDefault();
-//     login(email, password);
-//   };
-
-//   return (
-//     <form onSubmit={handleSubmit}>
-//       <label>
-//         Email:
-//         <input
-//           type="email"
-//           value={email}
-//           onChange={(e) => setEmail(e.target.value)}
-//         />
-//       </label>
-//       <label>
-//         Password:
-//         <input
-//           type="password"
-//           value={password}
-//           onChange={(e) => setPassword(e.target.value)}
-//         />
-//       </label>
-//       <button type="submit">Login</button>
-//     </form>
-//   );
-// }
-
-// export default LoginForm;
