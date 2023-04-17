904d2e43e0a107648229babecc148ad3c0f2d2d1
franGhisoni <ghisonifran@gmail.com>
2023-04-17 15:55:49 -0300

    backend integrado

--- src/Service/holdedConection.jsx
+++ src/Service/holdedConection.jsx
@@ -11,16 +11,12 @@ import React, { useState, useEffect, useCallback } from 'react';
 export default function useContactos(){
 const contactos= useCallback(async (email, password) => {
   //   aca lo que hago es remplazar los caracteres que por lo visto no se peuden mandar en un http request
-  const customid = `${email.replace(/@/g, "%40")}%3B${password}`;
-  //borrar
-  console.log(customid);
+ 
   //   todo en un bloque try para robustez
     try {
-        const response = await  fetch(`https://api.holded.com/api/invoicing/v1/contacts?customId=${customid}`, {
+        const response = await  fetch(`http://localhost:3000/v1/login?email=${email}&password=${password}`, {
           headers: {
             // todavia no esta resuelto lo del cors asi que el acces control no esta haciendo nada
-            'customid':customid,
-            'key': '343654e3d1014f792344a19ee8f40503',
             'accept': 'application/json',
             'Acces-Control-Allow-Origin': '*'
                     }
@@ -35,7 +31,7 @@ const contactos= useCallback(async (email, password) => {
         // borrar todos los console log
             console.log(data);
             console.log("data[0]");
-            console.log(data[0]);
+            console.log(data);
             console.log("JSON.stringify(data)");
             console.log(JSON.stringify(data));
             // guardo el user en un sessionStorgae
@@ -44,7 +40,7 @@ const contactos= useCallback(async (email, password) => {
             // sessionStorage.setItem("user", JSON.stringify(data[0]));
 
             // no elimine este por que fue el primer intentno y no estoy seguro si lo sigo usando en algun otro lugar
-            sessionStorage.setItem("userName", JSON.stringify(data[0].name));
+            sessionStorage.setItem("userName", JSON.stringify(data.name));
             console.log(data[0].name);
             console.log("sessionStorage.getItem('user')");
             console.log(sessionStorage.getItem("user"));
