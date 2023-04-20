8f831835fa325e0a7d65bdecca69e518cc3a4748
OtroAlien <flopysueiro1@gmail.com>
2023-04-20 19:55:51 -0300

    todo

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import ShopCSS from '../css/Shop.module.css';
 import Carousel from '../components/Carousel';
-import RadioInputs from '../components/InputsCompra';
+import Inputs from '../components/InputsCompra';
+import FAQ from '../components/PreguntasFrecuentes'
 
 function Shop() {
   return (
     <div className={ShopCSS.body}>
-      <div className={ShopCSS.carouselContainer}>
-        <Carousel />
+      <div className={ShopCSS.container}>
+        <div className={ShopCSS.carousel}>
+          <Carousel />
+        </div>
+        <div className={ShopCSS.inputs}>
+          <Inputs />
+        </div>
       </div>
-      <div className={ShopCSS.inputs}>
-        <RadioInputs />
+      <div>
+        <h1>Que incluye mi compra?</h1>
+        <br/>
       </div>
+      <div className={ShopCSS.faq}>
+      <h1>Preguntas Frecuentes</h1>
+        <FAQ/>
+      </div>
+
     </div>
   );
 }
