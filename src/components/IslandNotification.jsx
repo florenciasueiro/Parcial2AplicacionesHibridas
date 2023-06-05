c0cefea357c37e10baa10305adea18f98cd6f3ee
franGhisoni <ghisonifran@gmail.com>
2023-06-05 11:53:34 -0300

    compra notification en dynamic island

--- src/components/IslandNotification.jsx
+++ src/components/IslandNotification.jsx
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import IslandNotificationCSS from '../css/IslandNotification.module.css';
 
-function IslandNotification({ playAnimation }) {
+function IslandNotification({ playAnimation, message }) {
 
 
   return (
     <div className={`${IslandNotificationCSS.container} ${playAnimation ? IslandNotificationCSS.show : ''}`}>
-      <div className={`${IslandNotificationCSS.notification} ${playAnimation ? IslandNotificationCSS.playAnimation : ''}`}>
-        <h3>Notificacion</h3>
+      <div style={{textAlign:"center"}}className={`${IslandNotificationCSS.notification} ${playAnimation ? IslandNotificationCSS.playAnimation : ''}`}>
+        <br/>
+        <br/>
+        <h3 style={{color:"black"}}>{message}</h3>
+        <br/>
       </div>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,13 +7,105 @@ import NavbarBlackCSS from '../css/NavbarBlack.module.css';
 import IslandNotification from './IslandNotification';
 import useContactos from '../Service/APIlogin'
 
+const postVenta = async (venta) => {
+  try {
+    const response = await fetch(`http://localhost:8080/v1/venta`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(venta)
+    });
+    
+    if (response.status > 200 || response.status <= 300) {
+      console.log(response.status)
+      console.log(await response.json());
+          if(response.status === 201){
+              
+              return response.status;
+          }
+          else if (response.status === 400){
+              
+              return response.status;
+          }
+          else if (response.status === 409){
+
+              return response.status;
+          }
+          else if (response.status === 500){
+
+              return response.status;
+          }
+    }
+    
+  else if (response.status !==200){
+      let errorMessage = 'Ha ocurrido un error.';
+      throw new Error(errorMessage);
+  }
+  else if (response.status === 400) {
+    let errorMessage = 'Lote altan datos requeridos en la solicitud.';
+      throw new Error(errorMessage);
+      } 
+  else if (response.status === 409) {
+    let errorMessage = 'El usuario ya ha sido registrado previamente.';
+        throw new Error(errorMessage);
+      }
+      
+    
+  
+    const data = await response.json();
+    console.log("se ejecuto registro");
+    console.log(data);
+  
+
+} catch (error) {
+console.error(error.message);
+console.error(error.status);
+    // Puedes manejar el error de otra manera, por ejemplo, mostrar un mensaje de error en la aplicación.
+  }
+}
+
+const urlParams = new URLSearchParams(window.location.search);
+const status = Object.fromEntries(urlParams).status;
+console.log(Object.fromEntries(urlParams))
+
 function Navbar() {
   const contactos = useContactos();
   const location = useLocation();
   const isShopPage = location.pathname === '/shop';
-
+  const compraJson = sessionStorage.getItem('compra');
+  const compra = compraJson ? JSON.parse(compraJson) : null;
   const usuarioJson = sessionStorage.getItem('user');
   const usuario = usuarioJson ? JSON.parse(usuarioJson) : null;
+
+
+ 
+  
+  useEffect(() => {
+    const compraJson = sessionStorage.getItem('compra');
+    const compraCounter = Number(sessionStorage.getItem('compraCounter'));
+    console.log(compraCounter);
+    if (compraJson && compraCounter === 0) {
+      setPlayAnimation(true);
+      sessionStorage.setItem('compraCounter', '1');
+      if (status === 'approved' && compra) {
+        // message = 'TU COMPRA ESTÁ APROBADA';
+        setMessage(`TU COMPRA ESTÁ APROBADA `);
+        postVenta(compra);
+        console.log(compra);
+    
+      } else if (status === 'failure') {
+        setMessage('TU COMPRA ESTÁ RECHAZADA');
+      } else if (status === 'pending') {
+        setMessage('TU COMPRA ESTÁ EN PROCESO');
+      }
+      
+      setTimeout(() => {
+        setPlayAnimation(false);
+      }, 3000);
+    }
+  }, []); 
+
 
   const displayName = (props) => {
     if (!usuario || Object.keys(usuario).length === 0) {
@@ -28,6 +120,7 @@ function Navbar() {
       );
     }
   };
+ 
 
   const handleLogin = (email, password) => {
     Contactos(email, password);
@@ -41,8 +134,9 @@ function Navbar() {
   const [showLogin, setShowLogin] = useState(false);
   const [showNotification, setShowNotification] = useState(false);
   const [playAnimation, setPlayAnimation] = useState(false);
+  const [message, setMessage] = useState('');
   const loginRef = useRef(null);
-
+ 
   const showLoginDropdown = () => {
     setShowLogin(true);
   };
@@ -58,18 +152,26 @@ function Navbar() {
   const handleDropdownMouseLeave = () => {
     setTimeout(() => {
       setShowLogin(false);
-    }, 60000); // Retrasar la ocultación del login en 600 milisegundos (0.6 segundos)
+    }, 6000); // Retrasar la ocultación del login en 600 milisegundos (0.6 segundos)
   };
 
   useEffect(() => {
     const usuarioJson = sessionStorage.getItem('user');
-    if (usuarioJson) {
+    const welcomeCounter = Number(sessionStorage.getItem('welcomeCounter'));
+    
+    if (usuarioJson && welcomeCounter === 0) {
       setPlayAnimation(true);
+      sessionStorage.setItem('welcomeCounter', '1');
+      setMessage('Bienvenido');
       setTimeout(() => {
         setPlayAnimation(false);
-      }, 600000000);
+      }, 3000);
     }
-  }, []);
+  }, []); 
+
+
+
+
 
   return (
     <div className={`${NavbarCSS.Navbarcontainer} ${isShopPage ? NavbarBlackCSS.Navbarcontainer : ''}`}>
@@ -110,7 +212,7 @@ function Navbar() {
             <Login Contactos={handleLogin} />
           </div>
         </div>
-          {<IslandNotification playAnimation={playAnimation} />}
+          {<IslandNotification playAnimation={playAnimation} message={message} />}
       </div>
     </div>
   );
