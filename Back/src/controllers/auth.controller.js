ce2abdaa9eb175632ed961b1e81d17f79c3fc0e8
florenciasueiro <florencia.sueiro@davinci.edu.ar>
2024-07-05 20:09:07 -0300

    Más backend

--- Back/src/controllers/auth.controller.js
+++ Back/src/controllers/auth.controller.js
@@ -1,3 +1,28 @@
-export const register = (req, res) => res.send('register');
-export const login = (req, res) => res.send('login');
+import User from '../models/user.model.js';
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
+export const register = async (req, res) => {
+    const {email, password, username} = req.body
+
+    try{
+        const passwordHash = await bcrypt.hash(password, 10)
+        const newUser = new User({
+            username,
+            email,
+            password: passwordHash,
+        })
+    
+        const userSaved = await newUser.save();
+        res.json({
+            id: userSaved._id,
+            username: userSaved.username,
+            email: userSaved.email,
+            createAt: userSaved.createdAt
+        })
+
+    }catch(error){
+        console.log(error);
+    }
+};
+export const login = (req, res) => res.send('login');
