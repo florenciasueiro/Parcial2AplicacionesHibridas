8c0f67cdbc41563357f68f7c32fa71015e10c27e
Facundo <facundo.paez@davinci.edu.ar>
2024-07-06 05:33:38 -0300

    Fix headings + Carrusel Eventos Beta

--- Front/src/pages/Quarters.js
+++ Front/src/pages/Quarters.js
@@ -15,6 +15,8 @@ import {
   CardGrid25,
 } from '../components/GridApp';
 import GridCSS from '../css/Grid.module.css';
+import 'bootstrap/dist/css/bootstrap.css'; 
+import Carousel from 'react-bootstrap/Carousel'; 
 
 function Quarters() {
   const [scrollY, setScrollY] = useState(0);
@@ -46,6 +48,33 @@ function Quarters() {
           </div>
           {/* <BackgroundQuarters className={QuartersCSS.background} /> */} 
         </>
+        <div style={{ display: 'block', width: 700, padding: 30 }}> 
+          <h4>React-Bootstrap Carousel Component</h4> 
+          <Carousel> 
+            <Carousel.Item interval={1500}> 
+              <img 
+                className="d-block w-100"
+                src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122739/2-300x115.png"
+                alt="Image One"
+              /> 
+              <Carousel.Caption> 
+                <h3>Label for first slide</h3> 
+                <p>Sample Text for Image One</p> 
+              </Carousel.Caption> 
+            </Carousel.Item> 
+            <Carousel.Item interval={500}> 
+              <img 
+                className="d-block w-100"
+                src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122716/1-300x115.png"
+                alt="Image Two"
+              /> 
+              <Carousel.Caption> 
+                <h3>Label for second slide</h3> 
+                <p>Sample Text for Image Two</p> 
+              </Carousel.Caption> 
+            </Carousel.Item> 
+          </Carousel> 
+        </div>
         <div className={GridCSS.grilla}>
           <div
             className={`${GridCSS.tarjetas} ${GridCSS.tarjeta1} ${
