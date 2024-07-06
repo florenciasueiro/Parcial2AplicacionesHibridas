09bc5b44beb0962ce3b9e7d23ce6a2d0c994de7f
Facundo <facundo.paez@davinci.edu.ar>
2024-07-06 04:35:26 -0300

    Carrusel beta + Navbar fixes

--- Front/src/pages/Eventos.js
+++ Front/src/pages/Eventos.js
@@ -1,30 +1,35 @@
-import React from 'react';
-import InicioCSS from '../css/Inicio.module.css';
-import Cards from '../components/Cards';
-import {CardGrid26} from '../components/GridApp';
-import { BackgroundQuarters } from  '../components/Background';
-
-function Eventos() {
-  return (
-    <div className={InicioCSS.inicio}>
-      {/* <a href="/quarters" className={InicioCSS.background}>
-      <div className={InicioCSS.textEvents}>
-        <div className={InicioCSS.logoP}>
-          <img className={InicioCSS.img} src={"/img/LogoBlanco.png"}/>
-          <p className={InicioCSS.p}>Events</p>
-        </div>
-        <p className={InicioCSS.pEvent}>Hogar de eventos especiales que anuncian lo último en productos, servicios y más.</p>
-        <button className={InicioCSS.button}>Registrarse</button>
-      </div>
-        <BackgroundQuarters />
-      </a>   */}
-
-      <div>
-        <CardGrid26/>
-      </div>
-    </div>
-  );
+import React from 'react'; 
+import 'bootstrap/dist/css/bootstrap.css'; 
+import Carousel from 'react-bootstrap/Carousel'; 
+  
+export default function App() { 
+  return ( 
+    <div style={{ display: 'block', width: 700, padding: 30 }}> 
+      <h4>React-Bootstrap Carousel Component</h4> 
+      <Carousel> 
+        <Carousel.Item interval={1500}> 
+          <img 
+            className="d-block w-100"
+src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122739/2-300x115.png"
+            alt="Image One"
+          /> 
+          <Carousel.Caption> 
+            <h3>Label for first slide</h3> 
+            <p>Sample Text for Image One</p> 
+          </Carousel.Caption> 
+        </Carousel.Item> 
+        <Carousel.Item interval={500}> 
+          <img 
+            className="d-block w-100"
+src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122716/1-300x115.png"
+            alt="Image Two"
+          /> 
+          <Carousel.Caption> 
+            <h3>Label for second slide</h3> 
+            <p>Sample Text for Image Two</p> 
+          </Carousel.Caption> 
+        </Carousel.Item> 
+      </Carousel> 
+    </div> 
+  ); 
 }
-
-export default Eventos;
-
